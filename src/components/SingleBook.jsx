ad47c0f3bb5bd06f370f0fc05c176cddb564b490
blade1995 <krnicola1995@gmail.com>
2021-06-02 15:42:27 +0200

    task 6 done

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,24 @@
 import { Card, Col } from 'react-bootstrap'
 import React from 'react'
+import MyBadge from './MyBadge'
 
 
-const SingleBook = (props) => (
-  <Col>
-  <Card>
-        <Card.Img variant="top" src={props.img} />
+class SingleBook extends React.Component {
+
+    state = {
+        selected:false
+    }
+
+render () {
+ return <Col>
+  <Card onClick={()=> this.setState({selected:!this.state.selected})}>
+        <Card.Img variant="top" src={this.props.img} />
         <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
+            {this.state.selected? <MyBadge text={this.props.category} variant='danger'/> : null}
+            <Card.Title>{this.props.title}</Card.Title>
         </Card.Body>
     </Card>
     </Col>
-)
 
+}}
 export default SingleBook
