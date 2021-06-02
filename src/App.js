c7fe5e9554aab5570d186cd94556ecce630f8b87
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-02 14:24:13 +0100

    ex5 done

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
     <div className="App">
       <WarningSign text="HELLO" />
       <MyBadge text='Fantasy' variant='danger'/>
-      {/* {
-        Fantasy.map(book => <SingleBook title={book.title} img={book.img} />)
-      } */}
-      <BookList books={Fantasy}/>
+      <BookList books={Fantasy} />
     </div>
   );
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,11 +2,27 @@ import {Container, Row} from 'react-bootstrap'
 import React from 'react'
 import SingleBook from './SingleBook'
 
-const BookList =(props) =>(
-    <Container>
-        <Row>
-     {props.books.map(book => <SingleBook {...book} />)}
-        </Row>
-    </Container>
-)
+class BookList extends React.Component {
+
+    state = {
+        filteredBooks: this.props.books
+    }
+
+    filterBooks = (query) => {
+        const newArray = this.props.books.filter(book => this.state.query === '' ? true : book.title.toLowerCase().includes(query.toLowerCase()))
+        this.setState({ filteredBooks: newArray})
+    }
+
+    render() {
+        return <Container>
+                    <Row>
+                        Search: <input type="text" onChange={(e) => this.filterBooks(e.target.value)} />
+                    </Row>
+                    <Row>
+                        {this.state.filteredBooks.map(book => <SingleBook {...book} />)}
+                    </Row>
+                </Container>
+    }
+}
+
 export default BookList
