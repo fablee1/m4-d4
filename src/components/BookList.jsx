853810cc55e974799005407adb3d6b729b666ed2
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-08 16:42:37 +0100

    Finished homework for d7

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -10,15 +10,11 @@ const BookList = (props) => {
     const [filteredBooks, setFilteredBooks] = useState(props.books)
     const [commentsOnRight, setCommentsOnRight] = useState(true)
 
-    const previousBooks = useRef()
-    useEffect(() => previousBooks.current = filteredBooks)
-    const prevBooks = previousBooks.current
-
-    const searchInput = useRef(null)
+    const searchInput = useRef()
 
     useEffect(() => {
-        (prevBooks[0].asin !== props.books[0].asin) && filterBooks(searchInput.value)
-    })
+        filterBooks(searchInput.current.value)
+    }, [props.books])
 
     const filterBooks = (query) => {
         const newArray = props.books.filter(book => query === '' ? true : book.title.toLowerCase().includes(query.toLowerCase()))
@@ -37,7 +33,7 @@ const BookList = (props) => {
     return (<Container>
         <Row className="justify-content-center">
             <Col xs={12} className='d-flex justify-content-center my-4'>
-                <span className="mr-3">Search:</span><input className='w-100' type="text" ref={e => searchInput.current = e} onChange={(e) => filterBooks(e.target.value)} />
+                <span className="mr-3">Search:</span><input className='w-100' type="text" ref={searchInput} onChange={(e) => filterBooks(e.target.value)} />
                 <Button className="ml-5 w-50" onClick={toggleComments}>Card Comments</Button>
             </Col>
         </Row>
